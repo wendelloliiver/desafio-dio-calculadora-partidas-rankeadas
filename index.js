57a12adeb1d104c9e925fa44d06aea59fe88e96d
Wendell Oliveira <68166732+wendelloliiver@users.noreply.github.com>
2024-07-13 20:58:46 -0300

    atualizando o desafio!

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Vasco!")
+function saldo(vitoria, derrota) {
+    let nivel
+    let rank = (vitoria - derrota)
+    
+    if (rank <= 10) {
+        nivel = "Ferro"
+    } else if (rank > 10 && rank <= 20) {
+        nivel = "Bronze"
+    } else if (rank > 20 && rank <= 50) {
+        nivel = "Prata"
+    } else if (rank > 50 && rank <= 80) {
+        nivel = "Ouro"
+    } else if (rank > 80 && rank <= 90) {
+        nivel = "Diamante"
+    } else if (rank > 90 && rank <= 100) {
+        nivel = "Lendário"
+    } else if (rank >= 101) {
+        nivel = "Imortal"
+    }
+
+    console.log("O Herói tem de saldo de " + rank + " está no nível de " + nivel)
+}
+
+saldo(92, 10)
